09b927b8aacd9a4a57562c45d372fbc2814537d3
R0L02796 <arolandelli@itba.edu.ar>
2025-05-18 20:42:17 -0300

    repaired speciality filter of announcements

--- src/main/kotlin/ar/edu/itba/harmos/persistence/AnnouncementRepository.kt
+++ src/main/kotlin/ar/edu/itba/harmos/persistence/AnnouncementRepository.kt
@@ -7,11 +7,19 @@ import org.springframework.data.domain.Page
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 @Repository
 interface AnnouncementRepository : PagingAndSortingRepository<Announcement, Long> {
     fun findByCreatedBy(appUser: AppUser, pageable: Pageable): Page<Announcement>
 
-    fun findBySpecialtiesIn(specialties: List<String>, pageable: Pageable): Page<Announcement>
-
+    @Query("""
+        SELECT a FROM Announcement a 
+        WHERE EXISTS (
+            SELECT 1 FROM a.specialties s 
+            WHERE s.name IN :specialties
+        )
+    """)
+    fun findBySpecialtiesIn(@Param("specialties") specialties: List<String>, pageable: Pageable): Page<Announcement>
 }
